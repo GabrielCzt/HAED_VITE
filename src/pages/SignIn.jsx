9701f98a71342954ad4e76332865246648c8baa3
AlexAlar69 <jose@MacBook-Air-de-Jose.local>
2023-06-12 12:54:41 -0600

    Nuevas funcionalidades

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useRef } from "react";
 import "../estilos/Login.css"; //Se reciclaron los estilos de la página Login, pues son exactamente los mismos
 import "../estilos/Pages.css";
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -12,14 +12,22 @@ const cookie = new Cookies();
 
 
 
-function Sign(){
+function Sign() {
+
+
     //Código para la redirección validada por sesión
     const navigate = new useNavigate();
     useEffect(() => {
-        if(cookie.get('nombres')){
-          navigate('/Perfil')
+        if (cookie.get('nombres')) {
+            navigate('/Perfil')
         }
     }, []);
+
+    //Referencia para enfocarse en 'input' de nombre
+    const nameRef = useRef()
+    useEffect(() => {
+        nameRef.current.focus();
+    }, [])
 
     //Código para el registro
     const [_nombres, set_Nombres] = useState("");
@@ -29,126 +37,126 @@ function Sign(){
     const [_password, set_Password] = useState("");
     const [_confirmation, set_Confirmation] = useState("");
 
-    const changeNombres = (event) =>{
+    const changeNombres = (event) => {
         set_Nombres(event.target.value)
     }
-    const changeApellidos = (event)=>{
+    const changeApellidos = (event) => {
         set_Apellidos(event.target.value)
     }
-    const changeMatricula =(event)=>{
+    const changeMatricula = (event) => {
         set_Matricula(event.target.value)
     }
-    const changeEmail =(event)=>{
+    const changeEmail = (event) => {
         set_Email(event.target.value)
     }
-    const changePassword = (event)=>{
+    const changePassword = (event) => {
         set_Password(event.target.value)
     }
 
-    const changeConfirmation =(event)=>{
+    const changeConfirmation = (event) => {
         set_Confirmation(event.target.value)
     }
     const [_error, set_Error] = useState("");
 
 
-    const handleSign =(event)=>{
+    const handleSign = (event) => {
         event.preventDefault();
 
-        if(_password === _confirmation){
+        if (_password === _confirmation) {
             const params = {
-                "nombres": _nombres  ,
-                "apellidos":  _apellidos ,
-                "matricula":_matricula,
-                "email":_email,
-                "password":_password,
-                "password_confirmation":_confirmation
+                "nombres": _nombres,
+                "apellidos": _apellidos,
+                "matricula": _matricula,
+                "email": _email,
+                "password": _password,
+                "password_confirmation": _confirmation
             }
-    
+
             axios.post("http://api-haed.danielreyesepitacio.cloud/api/auth/register", params)
-            .then(response =>{
-                console.log(response.data)
-                const logParams ={
-                    email: _email,
-                    password: _password
-                }
-                axios.post('http://api-haed.danielreyesepitacio.cloud/api/auth/login', logParams)
-                    .then(response =>{   
-                        //console.log(response.data)
-                    return response.data;                 
-                    })
-                    .then(response =>{
+                .then(response => {
+                    console.log(response.data)
+                    const logParams = {
+                        email: _email,
+                        password: _password
+                    }
+                    axios.post('http://api-haed.danielreyesepitacio.cloud/api/auth/login', logParams)
+                        .then(response => {
+                            //console.log(response.data)
+                            return response.data;
+                        })
+                        .then(response => {
                             let respuesta = response.data;
-                            cookie.set('administrador', respuesta.administrador, {path:"/"})
-                            cookie.set('apellidos', respuesta.apellidos, {path:"/"})
-                            cookie.set('centro_trabajo', respuesta.centro_trabajo, {path:"/"})
-                            cookie.set('id', respuesta.id, {path:"/"})
-                            cookie.set('email', respuesta.email, {path:"/"})                    
-                            cookie.set('matricula', respuesta.matricula, {path:"/"})
-                            cookie.set('nombres', respuesta.nombres, {path:"/"})
+                            cookie.set('administrador', respuesta.administrador, { path: "/" })
+                            cookie.set('apellidos', respuesta.apellidos, { path: "/" })
+                            cookie.set('centro_trabajo', respuesta.centro_trabajo, { path: "/" })
+                            cookie.set('id', respuesta.id, { path: "/" })
+                            cookie.set('email', respuesta.email, { path: "/" })
+                            cookie.set('matricula', respuesta.matricula, { path: "/" })
+                            cookie.set('nombres', respuesta.nombres, { path: "/" })
                             navigate("/Perfil");
 
-                    })
-            
-             })
-            .catch(error =>{
-                if(error.response && error.response.status === 422){
-                    console.log(error);
-                    set_Error("Ese usuario ya fue registrado");
-                }
-                else{
-                    console.log(error)
-                    set_Error("Algo salió mal, vuelva a intentarlo");
-                }
-            })
+                        })
+
+                })
+                .catch(error => {
+                    if (error.response && error.response.status === 422) {
+                        console.log(error);
+                        set_Error("Ese usuario ya fue registrado");
+                    }
+                    else {
+                        console.log(error)
+                        set_Error("Algo salió mal, vuelva a intentarlo");
+                    }
+                })
         }
-        else{
+        else {
             set_Error("Las contraseñas no coinciden")
         }
-        
+
     }
 
-    
 
 
 
 
 
-    return(        
+
+    return (
         <>
-        {/**Barra de titulo */}
-        <div className="barContains">
-            <div className="bar">               
-                <span className="display-3"><b>Registrarse</b></span>                
-            </div>
-        </div> 
-        {/**Etiqueta separadora de estilos */}
-        <div className="log">
-        <div className="container" id="log_sign">
-            <div className="row">
-                {/**Cuadro de la izquierda para iniciar sesión */}
-                <div className="col-sm-12 col-md-6 sign">
-                    <h2>¿Ya tienes una cuenta?</h2>
-                    <p>Para contestar la autoevaluación debes iniciar sesión, si ya tienes una cuenta da click en el botón Iniciar Sesión</p><br/>
-                    <Link to="/Iniciar-sesion" id="button">INICIAR SESIÓN</Link>
-                </div>
-                {/**Formulario para registro */}
-                <div className="col-sm-12 col-md-6 login">
-                    <h2>Bienvenido</h2>
-                    <h3>Vamos a crear una nueva cuenta</h3>
-                    <p id="error">{_error}</p>
-                    <form onSubmit={handleSign}>
-                        <input required value={_nombres} onChange={changeNombres} type={"text"} placeholder="Ingrese sus Nombres" ></input>
-                        <input required value={_apellidos} type={"text"} onChange={changeApellidos} placeholder="Ingrese sus Apellidos"></input>
-                        <input required value={_matricula} type={"text"} onChange={changeMatricula} placeholder="Coloque aquí su Matricula"></input>
-                        <input required value={_email}  type={"email"} onChange={changeEmail} placeholder="Ingrese su Correo Electrónico"></input>
-                        <input required value={_password} type={"password"} onChange={changePassword} placeholder="Contraseña"></input>
-                        <input required value={_confirmation} type={"password"} onChange={changeConfirmation} placeholder="Repita su contraseña"></input>
-                        <button type="submit" id="ingresar">CLICK AQUÍ PARA CREAR SU CUENTA</button>
-                    </form>
+            {/**Barra de titulo */}
+            <div className="barContains">
+                <div className="bar">
+                    <span className="display-3"><b>Registrarse</b></span>
                 </div>
             </div>
-        </div>
-        </div>
+            {/**Etiqueta separadora de estilos */}
+            <div className="log">
+                <div className="container" id="log_sign">
+                    <div className="row">
+                        {/**Cuadro de la izquierda para iniciar sesión */}
+                        <div className="col-sm-12 col-md-6 sign">
+                            <h2>¿Ya tienes una cuenta?</h2>
+                            <p>Para contestar la autoevaluación debes iniciar sesión, si ya tienes una cuenta da click en el botón Iniciar Sesión</p><br />
+                            <Link to="/Iniciar-sesion" id="button">INICIAR SESIÓN</Link>
+                        </div>
+                        {/**Formulario para registro */}
+                        <div className="col-sm-12 col-md-6 login">
+                            <h2>Bienvenido</h2>
+                            <h3>Vamos a crear una nueva cuenta</h3>
+                            <p id="error">{_error}</p>
+                            <form onSubmit={handleSign}>
+                                <input required value={_nombres} onChange={changeNombres} type={"text"} ref={nameRef} placeholder="Ingrese sus Nombres" ></input>
+                                <input required value={_apellidos} type={"text"} onChange={changeApellidos} placeholder="Ingrese sus Apellidos"></input>
+                                <input required value={_matricula} type={"text"} onChange={changeMatricula} placeholder="Coloque aquí su Matricula"></input>
+                                <input required value={_email} type={"email"} onChange={changeEmail} placeholder="Ingrese su Correo Electrónico"></input>
+                                <input required value={_password} type={"password"} onChange={changePassword} placeholder="Contraseña"></input>
+                                <input required value={_confirmation} type={"password"} onChange={changeConfirmation} placeholder="Repita su contraseña"></input>
+                                <button type="submit" id="ingresar">CLICK AQUÍ PARA CREAR SU CUENTA</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
