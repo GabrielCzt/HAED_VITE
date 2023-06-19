1161a0e3660446aac05fc5e6e92535f84da3e77d
AlexAlar69 <jose@MacBook-Air-de-Jose.local>
2023-06-19 11:02:02 -0600

    Registro con Efectos (Sipneer y Ojo)

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import "../estilos/Login.css"; //Se reciclaron los estilos de la página Login, pues son exactamente los mismos
 import "../estilos/Pages.css";
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Spinner } from 'react-bootstrap';
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useNavigate } from "react-router-dom";
 import Cookies from 'universal-cookie'
 import { useState } from "react";
@@ -11,9 +15,36 @@ const cookie = new Cookies();
 
 
 
-function Sign(){
+function Sign() {
     //Código para la redirección validada por sesión
     const navigate = new useNavigate();
+
+    {/*Estado indicador en la carga correcta de credenciales*/ }
+    const [loading, setLoading] = useState('')
+
+    {/*Estado indicador de error en la carga de credenciales*/ }
+    const [_error, set_Error] = useState("");
+
+
+    {/*//Estado para mostrar contraseña*/ }
+    const [ojos, setOjos] = useState(faEye)
+
+    {/*Muestra y oculta contraseña del input 'password' */ }
+    const SeePassword = () => {
+        let pass = document.getElementById("password");
+        let pass2 = document.getElementById("confirmation");
+        if (pass.type === "password" || pass2.type === "password") {
+            pass.type = "text";
+            pass2.type = "text"
+            setOjos(faEyeSlash);
+        }
+        else if (pass.type === "text" || pass2.type === "text") {
+            pass.type = "password";
+            pass2.type = "password"
+            setOjos(faEye);
+        }
+    }
+
     useEffect(() => {
         if (cookie.get('nombres')) {
             navigate('/Perfil')
@@ -28,7 +59,7 @@ function Sign(){
     const [_password, set_Password] = useState("");
     const [_confirmation, set_Confirmation] = useState("");
 
-    const changeNombres = (event) =>{
+    const changeNombres = (event) => {
         set_Nombres(event.target.value)
     }
     const changeApellidos = (event) => {
@@ -44,94 +75,95 @@ function Sign(){
         set_Password(event.target.value)
     }
 
-    const changeConfirmation =(event)=>{
+    const changeConfirmation = (event) => {
         set_Confirmation(event.target.value)
     }
-    const [_error, set_Error] = useState("");
 
-    const validarCadenas = (cadena) =>{
+    const validarCadenas = (cadena) => {
         const regex = /^[a-zA-Z\sáéíóúñÁÉÍÓÚ]+$/;
         return regex.test(cadena);
     }
 
-    const validarMatricula =(cadena)=>{
+    const validarMatricula = (cadena) => {
         const regex = /^utp/i;
         return regex.test(cadena)
     }
-    const handleSign =(event)=>{
+    const handleSign = (event) => {
         event.preventDefault();
-        if(validarCadenas(_nombres)){
-            if(validarCadenas(_apellidos)){
-                if(validarMatricula(_matricula)){
-                    if(_password === _confirmation){
+        if (validarCadenas(_nombres)) {
+            if (validarCadenas(_apellidos)) {
+                if (validarMatricula(_matricula)) {
+                    if (_password === _confirmation) {
+                        setLoading(true)
                         const params = {
-                            "nombres": _nombres  ,
-                            "apellidos":  _apellidos ,
-                            "matricula":_matricula.toUpperCase(),
-                            "email":_email,
-                            "password":_password,
-                            "password_confirmation":_confirmation
+                            "nombres": _nombres,
+                            "apellidos": _apellidos,
+                            "matricula": _matricula.toUpperCase(),
+                            "email": _email,
+                            "password": _password,
+                            "password_confirmation": _confirmation
                         }
-                
+
                         axios.post("http://api-haed.danielreyesepitacio.cloud/api/auth/register", params)
-                        .then(response =>{
-                            console.log(response.data)
-                            const logParams ={
-                                email: _email,
-                                password: _password
-                            }
-                            axios.post('http://api-haed.danielreyesepitacio.cloud/api/auth/login', logParams)
-                                .then(response =>{   
-                                    //console.log(response.data)
-                                return response.data;                 
-                                })
-                                .then(response =>{
+                            .then(response => {
+                                console.log(response.data)
+                                const logParams = {
+                                    email: _email,
+                                    password: _password
+                                }
+                                axios.post('http://api-haed.danielreyesepitacio.cloud/api/auth/login', logParams)
+                                    .then(response => {
+                                        //console.log(response.data)
+                                        return response.data;
+                                    })
+                                    .then(response => {
                                         let respuesta = response.data;
-                                        cookie.set('administrador', respuesta.administrador, {path:"/"})
-                                        cookie.set('apellidos', respuesta.apellidos, {path:"/"})
-                                        cookie.set('centro_trabajo', respuesta.centro_trabajo, {path:"/"})
-                                        cookie.set('id', respuesta.id, {path:"/"})
-                                        cookie.set('email', respuesta.email, {path:"/"})                    
-                                        cookie.set('matricula', respuesta.matricula, {path:"/"})
-                                        cookie.set('nombres', respuesta.nombres, {path:"/"})
-                                        cookie.set('token', response.token, {path:"/"})
+                                        cookie.set('administrador', respuesta.administrador, { path: "/" })
+                                        cookie.set('apellidos', respuesta.apellidos, { path: "/" })
+                                        cookie.set('centro_trabajo', respuesta.centro_trabajo, { path: "/" })
+                                        cookie.set('id', respuesta.id, { path: "/" })
+                                        cookie.set('email', respuesta.email, { path: "/" })
+                                        cookie.set('matricula', respuesta.matricula, { path: "/" })
+                                        cookie.set('nombres', respuesta.nombres, { path: "/" })
+                                        cookie.set('token', response.token, { path: "/" })
+                                        cookie.set('Verified', ('verificado'), { path: "/" })
                                         setTimeout(() => navigate("/Perfil"), 1000)
-                                })
-                        
-                         })
-                        .catch(error =>{
-                            if(error.response && error.response.status === 422){
-                                console.log(error);
-                                set_Error("Ese usuario ya fue registrado");
-                            }
-                            else{
-                                console.log(error)
-                                set_Error("Algo salió mal, vuelva a intentarlo");
-                            }
-                        })
+                                    })
+
+                            })
+                            .catch(error => {
+                                if (error.response && error.response.status === 422) {
+                                    console.log(error);
+                                    set_Error("Ese usuario ya fue registrado");
+                                }
+                                else {
+                                    console.log(error)
+                                    set_Error("Algo salió mal, vuelva a intentarlo");
+                                }
+                            })
                     }
                     else {
                         set_Error("Las contraseñas no coinciden")
                         document.getElementById("confirmation").focus()
                     }
                 }
-                else{
+                else {
                     set_Error("Ingrese una matricula valida")
-            document.getElementById("matricula").focus()
+                    document.getElementById("matricula").focus()
                 }
             }
-            else{
+            else {
                 set_Error("Los apellidos no pueden tener números ni caracteres especiales")
                 document.getElementById('apellidos').focus()
             }
         }
-        else{
-            
+        else {
+
             set_Error("EL nombre no puede tener números ni caracteres especiales")
-                    document.getElementById('nombres').focus()
-        }  
+            document.getElementById('nombres').focus()
+        }
     }
-    return(        
+    return (
         <>
             {/**Barra de titulo */}
             <div className="barContains">
@@ -153,13 +185,34 @@ function Sign(){
                         <div className="col-sm-12 col-md-6 login">
                             <h2>Bienvenido</h2>
                             <h3>Vamos a crear una nueva cuenta</h3>
-                            <p id="error">{_error}</p>
+
+                            {loading ?
+                                (
+
+                                    <div
+                                        className="alert alert-success d-flex align-items-center"
+                                        role="alert"
+                                    >
+                                        <div>
+                                            <p className="text-credentials">Cargando credenciales</p>
+                                        </div>
+                                        <div className="Spin">
+                                            <Spinner animation="border" variant="success" />
+                                        </div>
+                                    </div>
+                                ) :
+                                (
+
+                                    <p id="error">{_error}</p>
+                                )}
+
                             <form onSubmit={handleSign}>
-                                <input id="nombres" required value={_nombres} onChange={changeNombres} type={"text"}  placeholder="Ingrese sus Nombres" ></input>
+                                <input id="nombres" required value={_nombres} onChange={changeNombres} type={"text"} placeholder="Ingrese sus Nombres" ></input>
                                 <input id="apellidos" required value={_apellidos} type={"text"} onChange={changeApellidos} placeholder="Ingrese sus Apellidos"></input>
                                 <input id="matricula" required value={_matricula} type={"text"} onChange={changeMatricula} placeholder="Coloque aquí su Matricula"></input>
                                 <input id="email" required value={_email} type={"email"} onChange={changeEmail} placeholder="Ingrese su Correo Electrónico"></input>
                                 <input id="password" required value={_password} type={"password"} onChange={changePassword} placeholder="Contraseña"></input>
+                                <FontAwesomeIcon id="eye" onClick={SeePassword} icon={ojos} />
                                 <input id="confirmation" required value={_confirmation} type={"password"} onChange={changeConfirmation} placeholder="Repita su contraseña"></input>
                                 <button type="submit" id="ingresar">CLICK AQUÍ PARA CREAR SU CUENTA</button>
                             </form>
